846a1da8616d4090e9ccb275b9fbba9f06936c51
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-02-16 17:31:46 +0200

    [cmake] Add generating version.hpp file with app version info
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/sys/printk.h>
+
+#include <aos/common/version.hpp>
+
+#include "version.hpp"
+
 int main(void)
 {
+    printk("*** Aos zephyr application: %s ***\n", AOS_ZEPHYR_APP_VERSION);
+    printk("*** Aos core library: %s ***\n", AOS_CORE_VERSION);
+
     return 0;
 }
