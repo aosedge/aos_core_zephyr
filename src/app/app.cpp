a6f831e9d4f1bf3cce1e76cdcb753f33218c204d
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-02-23 17:57:30 +0200

    [app] Add CMClient and its initialization
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -16,5 +16,10 @@ aos::Error App::Init()
 {
     LOG_INF() << "Initialize application";
 
+    auto err = mCMClient.Init();
+    if (!err.IsNone()) {
+        return err;
+    }
+
     return aos::ErrorEnum::eNone;
 }
