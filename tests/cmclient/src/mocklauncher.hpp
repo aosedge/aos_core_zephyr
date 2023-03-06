1eea88d97e9282195609a30421f2550eb6f16f6c
Yevgen Abramov <yevgen_abramov@epam.com>
2023-03-06 13:49:23 +0200

    [cmclient_test] Add test
    Signed-off-by: Yevgen Abramov <yevgen_abramov@epam.com>

---
+++ tests/cmclient/src/mocklauncher.hpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 Renesas Electronics Corporation.
+ * Copyright (C) 2023 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <aos/sm/launcher.hpp>
+
+using namespace aos;
+
+static constexpr size_t TestArraysSize = 10;
+
+class MockLauncher : public sm::launcher::LauncherItf {
+public:
+    MockLauncher(aos::ConditionalVariable& waitMessageCondVar, aos::Mutex& waitMessageMutex, bool& readyToProcess)
+        : mWaitMessageCondVar(waitMessageCondVar)
+        , mWaitMessageMutex(waitMessageMutex)
+        , mReadyToProcess(readyToProcess)
+    {
+    }
+
+    Error RunInstances(const Array<ServiceInfo>& services, const Array<LayerInfo>& layers,
+        const Array<InstanceInfo>& instances, bool forceRestart);
+
+    StaticArray<ServiceInfo, TestArraysSize>  mExpectedServices;
+    StaticArray<LayerInfo, TestArraysSize>    mExpectedLayers;
+    StaticArray<InstanceInfo, TestArraysSize> mExpectedInstances;
+    bool                                      mExpectedForceRestart;
+    aos::ConditionalVariable&                 mWaitMessageCondVar;
+    aos::Mutex&                               mWaitMessageMutex;
+    bool&                                     mReadyToProcess;
+};
