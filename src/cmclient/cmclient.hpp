cbab1504f2be905c11b59246c76b7af2dc9eeba8
Yevgen Abramov <yevgen_abramov@epam.com>
2023-03-06 13:35:02 +0200

    [cmclient] Add sending Nodeconfig and recive data from vchan
    Signed-off-by: Yevgen Abramov <yevgen_abramov@epam.com>

--- src/cmclient/cmclient.hpp
+++ src/cmclient/cmclient.hpp
@@ -8,8 +8,21 @@
 #ifndef CMCLIENT_HPP_
 #define CMCLIENT_HPP_
 
+#include "zephyr/sys/atomic.h"
+
+#include <aos/common/tools/buffer.hpp>
+#include <aos/common/tools/error.hpp>
 #include <aos/common/tools/thread.hpp>
+
 #include <aos/sm/launcher.hpp>
+
+#include <pb_encode.h>
+#include <servicemanager.pb.h>
+extern "C" {
+#include <vch.h>
+}
+
+using SHA256Digest = uint8_t[32];
 
 /**
  * CM client instance.
@@ -23,7 +36,13 @@ public:
         : mLauncher(nullptr)
         , mThread()
     {
+        atomic_clear_bit(&mFinishReadTrigger, 0);
     }
+
+    /**
+     * Destructor.
+     */
+    ~CMClient();
 
     /**
      * Initializes CM client instance.
@@ -49,11 +68,22 @@ public:
     aos::Error InstancesUpdateStatus(const aos::Array<aos::InstanceStatus>& instances) override;
 
 private:
-    aos::sm::launcher::LauncherItf* mLauncher;
-    aos::Mutex                      mMutex;
-    aos::Thread<>                   mThread;
+    aos::sm::launcher::LauncherItf*                              mLauncher;
+    aos::Thread<>                                                mThread;
+    atomic_t                                                     mFinishReadTrigger;
+    vch_handle                                                   mSMvchanHandler;
+    aos::StaticBuffer<servicemanager_v3_SMOutgoingMessages_size> mSendBuffer;
+    aos::StaticBuffer<servicemanager_v3_SMIncomingMessages_size> mReceiveBuffer;
+    servicemanager_v3_SMIncomingMessages                         mIncomingMessage;
+    servicemanager_v3_SMOutgoingMessages                         mOutgoingMessage;
+    aos::Mutex                                                   mSendMutex;
 
-    void ProcessMessages();
+    void       ProcessMessages();
+    void       ConnectToCM();
+    void       SendNodeConfiguration();
+    aos::Error SendPbMessageToVchan();
+    aos::Error SendBufferToVchan(vch_handle* vChanHandler, const uint8_t* buffer, size_t msgSize);
+    aos::Error CalculateSha256(const aos::Buffer& buffer, size_t size, SHA256Digest& digest);
 };
 
 #endif
