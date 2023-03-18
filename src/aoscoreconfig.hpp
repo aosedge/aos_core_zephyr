85389424ac780d995c5e91f8d8d335fc9c9f6e97
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-03-18 21:01:30 +0200

    [aoscoreconfig] Move aos core lib static config to separate file
    Keeping aos core lib configuration in file is more convenient to use and gives
    more flexibility and readability.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/aoscoreconfig.hpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2023 Renesas Electronics Corporation.
+ * Copyright (C) 2023 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOSCORECONFIG_HPP_
+#define AOSCORECONFIG_HPP_
+
+#ifndef CONFIG_BOARD_NATIVE_POSIX
+#define AOS_CONFIG_THREAD_DEFAULT_STACK_SIZE 1024
+#endif
+
+#endif
