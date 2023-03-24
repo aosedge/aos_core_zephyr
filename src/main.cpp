1a8ee2346f25a2678e01ba190839b9b523f05457
Sergiy Kibrik <Sergiy_Kibrik@epam.com>
2023-03-24 11:28:20 +0200

    [config] Add domain config to start DomD on Gen3 board from Zephyr
    Signed-off-by: Sergiy Kibrik <Sergiy_Kibrik@epam.com>
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>
    Acked-by: Mykola Kvach <mykola_kvach@epam.com>

--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,25 @@
 #include "logger/logger.hpp"
 #include "version.hpp"
 
-#if defined(CONFIG_SOC_SERIES_RCAR_GEN4)
+#if defined(CONFIG_SOC_SERIES_RCAR_GEN4) || defined(CONFIG_SOC_SERIES_RCAR_GEN3)
 extern "C" {
 #include <xen_dom_mgmt.h>
 
 extern struct xen_domain_cfg domd_cfg;
 }
-#endif /* CONFIG_SOC_SERIES_RCAR_GEN4 */
+#endif /* CONFIG_SOC_SERIES_RCAR_GEN4 || CONFIG_SOC_SERIES_RCAR_GEN3 */
 
 int main(void)
 {
     printk("*** Aos zephyr application: %s ***\n", AOS_ZEPHYR_APP_VERSION);
     printk("*** Aos core library: %s ***\n", AOS_CORE_VERSION);
 
-#if defined(CONFIG_SOC_SERIES_RCAR_GEN4)
+#if defined(CONFIG_SOC_SERIES_RCAR_GEN4) || defined(CONFIG_SOC_SERIES_RCAR_GEN3)
     int rc = domain_create(&domd_cfg, 1);
     if (rc) {
         printk("failed to create domain (%d)\n", rc);
     }
-#endif /* CONFIG_SOC_SERIES_RCAR_GEN4 */
+#endif /* CONFIG_SOC_SERIES_RCAR_GEN4 || CONFIG_SOC_SERIES_RCAR_GEN3 */
 
     Logger::Init();
 
