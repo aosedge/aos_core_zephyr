046a81cf86e3c5d9d6d7562942b66c1c8266275b
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2023-03-28 16:45:10 +0300

    Revert "[storage] Add mount and unmount commands of the mmc storage"
    This reverts commit ed241b036a59af023cf0ac32363deb717e06b8c4.

--- src/main.cpp
+++ src/main.cpp
@@ -15,37 +15,6 @@
 #include "logger/logger.hpp"
 #include "version.hpp"
 
-#if defined(CONFIG_FS_LITTLEFS_BLK_DEV) && defined(CONFIG_FILE_SYSTEM_LITTLEFS)
-#include <zephyr/fs/littlefs.h>
-FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(lfs_data);
-static struct fs_mount_t littlefs_mnt = {
-    .type = FS_LITTLEFS,
-    .mnt_point = "/aos:",
-    .fs_data = &lfs_data,
-    .storage_dev = (void *)CONFIG_SDMMC_VOLUME_NAME,
-    .flags = FS_MOUNT_FLAG_USE_DISK_ACCESS
-};
-
-/*
- * NOTE: Unmount function should not be implemented.
- * This because we have one-shot application and we do not expect
- * to have some reset of shutdown procedure.
- */
-static int mount_mmc()
-{
-    int rc = fs_mount(&littlefs_mnt);
-
-    if (rc != 0) {
-        printk("Error mounting %s:%d\n", littlefs_mnt.mnt_point, rc);
-        return -ENOEXEC;
-    }
-
-    return 0;
-}
-#else
-#define mount_mmc()  0
-#endif /* CONFIG_FS_LITTLEFS_BLK_DEV && CONFIG_FILE_SYSTEM_LITTLEFS */
-
 #if defined(CONFIG_SOC_SERIES_RCAR_GEN4) || defined(CONFIG_SOC_SERIES_RCAR_GEN3)
 extern "C" {
 #include <xen_dom_mgmt.h>
@@ -60,16 +29,10 @@ int main(void)
     printk("*** Aos core library: %s ***\n", AOS_CORE_VERSION);
     printk("*** Aos core size: %lu ***\n", sizeof(App));
 
-    int rc = mount_mmc();
-    if (rc != 0) {
-        return rc;
-    }
-
 #if defined(CONFIG_SOC_SERIES_RCAR_GEN4) || defined(CONFIG_SOC_SERIES_RCAR_GEN3)
-    rc = domain_create(&domd_cfg, 1);
+    int rc = domain_create(&domd_cfg, 1);
     if (rc) {
         printk("failed to create domain (%d)\n", rc);
-        return rc;
     }
 #endif /* CONFIG_SOC_SERIES_RCAR_GEN4 || CONFIG_SOC_SERIES_RCAR_GEN3 */
 
