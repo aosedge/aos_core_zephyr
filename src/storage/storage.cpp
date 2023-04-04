43405764c64f91948bc036b5fe110d101783b43a
Mykola Solianko <mykola_solianko@epam.com>
2023-04-04 14:18:17 +0300

    [storage] Integrate filestorage database
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/storage/storage.cpp
+++ src/storage/storage.cpp
@@ -5,8 +5,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "storage.hpp"
+#include <aos/common/tools/fs.hpp>
+
 #include "log.hpp"
+#include "storage.hpp"
 
 /***********************************************************************************************************************
  * Public
@@ -16,25 +18,192 @@ aos::Error Storage::Init()
 {
     LOG_DBG() << "Initialize storage";
 
+    aos::Error err;
+
+    if (!(err = aos::FS::MakeDirAll(cStoragePath)).IsNone()) {
+        LOG_ERR() << "Failed to create storage directory: " << err;
+
+        return err;
+    }
+
+    LOG_DBG() << "Storage directory: " << cStoragePath;
+
+    auto instancePath = aos::FS::JoinPath(cStoragePath, "instance.db");
+
+    if (!(err = mInstanceDatabase.Init(instancePath)).IsNone()) {
+        return err;
+    }
+
+    auto servicePath = aos::FS::JoinPath(cStoragePath, "service.db");
+
+    if (!(err = mServiceDatabase.Init(servicePath)).IsNone()) {
+        return err;
+    }
+
     return aos::ErrorEnum::eNone;
 }
 
 aos::Error Storage::AddInstance(const aos::InstanceInfo& instance)
 {
-    return aos::ErrorEnum::eNone;
+    aos::LockGuard lock(mMutex);
+
+    return mInstanceDatabase.Add(ConvertInstanceInfo(instance));
 }
 
 aos::Error Storage::UpdateInstance(const aos::InstanceInfo& instance)
 {
+    aos::LockGuard lock(mMutex);
+
+    return mInstanceDatabase.Update(ConvertInstanceInfo(instance), [&instance](const InstanceInfo& data) {
+        return data.mInstanceIdent.mInstance == instance.mInstanceIdent.mInstance
+            && strcmp(data.mInstanceIdent.mSubjectID, instance.mInstanceIdent.mSubjectID.CStr()) == 0
+            && strcmp(data.mInstanceIdent.mServiceID, instance.mInstanceIdent.mServiceID.CStr()) == 0;
+    });
+
     return aos::ErrorEnum::eNone;
 }
 
 aos::Error Storage::RemoveInstance(const aos::InstanceIdent& instanceIdent)
 {
+    aos::LockGuard lock(mMutex);
+
+    return mInstanceDatabase.Remove([&instanceIdent](const InstanceInfo& data) {
+        return data.mInstanceIdent.mInstance == instanceIdent.mInstance
+            && strcmp(data.mInstanceIdent.mSubjectID, instanceIdent.mSubjectID.CStr()) == 0
+            && strcmp(data.mInstanceIdent.mServiceID, instanceIdent.mServiceID.CStr()) == 0;
+    });
+
     return aos::ErrorEnum::eNone;
 }
 
 aos::Error Storage::GetAllInstances(aos::Array<aos::InstanceInfo>& instances)
 {
-    return aos::ErrorEnum::eNone;
+    aos::LockGuard lock(mMutex);
+
+    auto err = mInstanceDatabase.ReadRecords([&instances, this](const InstanceInfo& instanceInfo) -> aos::Error {
+        auto instance = ConvertInstanceInfo(instanceInfo);
+        auto err = instances.PushBack(instance);
+        if (!err.IsNone()) {
+            return err;
+        }
+
+        return aos::ErrorEnum::eNone;
+    });
+
+    return err;
+}
+
+aos::Error Storage::AddService(const aos::sm::servicemanager::ServiceData& service)
+{
+    aos::LockGuard lock(mMutex);
+
+    return mServiceDatabase.Add(ConvertServiceData(service));
+}
+
+aos::Error Storage::UpdateService(const aos::sm::servicemanager::ServiceData& service)
+{
+    aos::LockGuard lock(mMutex);
+
+    return mServiceDatabase.Update(ConvertServiceData(service),
+        [&service](const ServiceData& data) { return strcmp(data.mServiceID, service.mServiceID.CStr()) == 0; });
+}
+
+aos::Error Storage::RemoveService(const aos::String& serviceID, uint64_t aosVersion)
+{
+    aos::LockGuard lock(mMutex);
+
+    return mServiceDatabase.Remove([&serviceID, aosVersion](const ServiceData& data) {
+        return strcmp(data.mServiceID, serviceID.CStr()) == 0 && data.mVersionInfo.mAosVersion == aosVersion;
+    });
+}
+
+aos::Error Storage::GetAllServices(aos::Array<aos::sm::servicemanager::ServiceData>& services)
+{
+    aos::LockGuard lock(mMutex);
+
+    auto err = mServiceDatabase.ReadRecords([&services, this](const ServiceData& serviceData) -> aos::Error {
+        auto service = ConvertServiceData(serviceData);
+        auto err = services.PushBack(service);
+        if (!err.IsNone()) {
+            return err;
+        }
+
+        return aos::ErrorEnum::eNone;
+    });
+
+    return err;
+}
+
+aos::RetWithError<aos::sm::servicemanager::ServiceData> Storage::GetService(const aos::String& serviceID)
+{
+    aos::LockGuard lock(mMutex);
+
+    ServiceData serviceData;
+
+    auto err = mServiceDatabase.ReadRecordByFilter(
+        serviceData, [&serviceID](const ServiceData& data) { return strcmp(data.mServiceID, serviceID.CStr()) == 0; });
+
+    if (!err.IsNone()) {
+        return aos::RetWithError<aos::sm::servicemanager::ServiceData>(aos::sm::servicemanager::ServiceData {}, err);
+    }
+
+    return aos::RetWithError<aos::sm::servicemanager::ServiceData>(ConvertServiceData(serviceData));
+}
+
+Storage::InstanceInfo Storage::ConvertInstanceInfo(const aos::InstanceInfo& instance)
+{
+    InstanceInfo instanceInfo;
+
+    instanceInfo.mInstanceIdent.mInstance = instance.mInstanceIdent.mInstance;
+    strcpy(instanceInfo.mInstanceIdent.mSubjectID, instance.mInstanceIdent.mSubjectID.CStr());
+    strcpy(instanceInfo.mInstanceIdent.mServiceID, instance.mInstanceIdent.mServiceID.CStr());
+    instanceInfo.mPriority = instance.mPriority;
+    strcpy(instanceInfo.mStatePath, instance.mStatePath.CStr());
+    strcpy(instanceInfo.mStoragePath, instance.mStoragePath.CStr());
+    instanceInfo.mUID = instance.mUID;
+
+    return instanceInfo;
+}
+
+aos::InstanceInfo Storage::ConvertInstanceInfo(const InstanceInfo& instance)
+{
+    aos::InstanceInfo instanceInfo;
+
+    instanceInfo.mInstanceIdent.mInstance = instance.mInstanceIdent.mInstance;
+    instanceInfo.mInstanceIdent.mSubjectID = instance.mInstanceIdent.mSubjectID;
+    instanceInfo.mInstanceIdent.mServiceID = instance.mInstanceIdent.mServiceID;
+    instanceInfo.mPriority = instance.mPriority;
+    instanceInfo.mStatePath = instance.mStatePath;
+    instanceInfo.mStoragePath = instance.mStoragePath;
+    instanceInfo.mUID = instance.mUID;
+
+    return instanceInfo;
+}
+
+Storage::ServiceData Storage::ConvertServiceData(const aos::sm::servicemanager::ServiceData& service)
+{
+    ServiceData serviceData;
+
+    serviceData.mVersionInfo.mAosVersion = service.mVersionInfo.mAosVersion;
+    strcpy(serviceData.mVersionInfo.mVendorVersion, service.mVersionInfo.mVendorVersion.CStr());
+    strcpy(serviceData.mVersionInfo.mDescription, service.mVersionInfo.mDescription.CStr());
+    strcpy(serviceData.mServiceID, service.mServiceID.CStr());
+    strcpy(serviceData.mProviderID, service.mProviderID.CStr());
+    strcpy(serviceData.mImagePath, service.mImagePath.CStr());
+
+    return serviceData;
+}
+
+aos::sm::servicemanager::ServiceData Storage::ConvertServiceData(const Storage::ServiceData& service)
+{
+    aos::sm::servicemanager::ServiceData serviceData;
+
+    serviceData.mVersionInfo.mAosVersion = service.mVersionInfo.mAosVersion;
+    serviceData.mVersionInfo.mVendorVersion = service.mVersionInfo.mVendorVersion;
+    serviceData.mVersionInfo.mDescription = service.mVersionInfo.mDescription;
+    serviceData.mServiceID = service.mServiceID;
+    serviceData.mProviderID = service.mProviderID;
+    serviceData.mImagePath = service.mImagePath;
+
+    return serviceData;
 }
--- src/storage/storage.hpp
+++ src/storage/storage.hpp
@@ -8,12 +8,18 @@
 #ifndef STORAGE_HPP_
 #define STORAGE_HPP_
 
+#include <aos/common/tools/thread.hpp>
 #include <aos/sm/launcher.hpp>
+#include <aos/sm/servicemanager.hpp>
+
+#include "filestorage.hpp"
 
 /**
  * Storage instance.
  */
-class Storage : public aos::sm::launcher::StorageItf, private aos::NonCopyable {
+class Storage : public aos::sm::launcher::StorageItf,
+                public aos::sm::servicemanager::StorageItf,
+                private aos::NonCopyable {
 public:
     /**
      * Initializes storage instance.
@@ -52,6 +58,86 @@ public:
      * @return Error.
      */
     aos::Error GetAllInstances(aos::Array<aos::InstanceInfo>& instances) override;
+
+    /**
+     * Adds new service to storage.
+     *
+     * @param service service to add.
+     * @return Error.
+     */
+    aos::Error AddService(const aos::sm::servicemanager::ServiceData& service) override;
+
+    /**
+     * Updates previously stored service.
+     *
+     * @param service service to update.
+     * @return Error.
+     */
+    aos::Error UpdateService(const aos::sm::servicemanager::ServiceData& service) override;
+
+    /**
+     * Removes previously stored service.
+     *
+     * @param serviceID service ID to remove.
+     * @param aosVersion Aos service version.
+     * @return Error.
+     */
+    aos::Error RemoveService(const aos::String& serviceID, uint64_t aosVersion) override;
+
+    /**
+     * Returns all stored services.
+     *
+     * @param services array to return stored services.
+     * @return Error.
+     */
+    aos::Error GetAllServices(aos::Array<aos::sm::servicemanager::ServiceData>& services) override;
+
+    /**
+     * Returns service data by service ID.
+     *
+     * @param serviceID service ID.
+     * @return  RetWithError<ServiceData>.
+     */
+    aos::RetWithError<aos::sm::servicemanager::ServiceData> GetService(const aos::String& serviceID) override;
+
+private:
+    constexpr static auto cStoragePath = CONFIG_AOS_STORAGE_DIR;
+
+    struct InstanceIdent {
+        char     mServiceID[aos::cServiceIDLen];
+        char     mSubjectID[aos::cSubjectIDLen];
+        uint64_t mInstance;
+    };
+
+    struct InstanceInfo {
+        InstanceIdent mInstanceIdent;
+        uint32_t      mUID;
+        uint64_t      mPriority;
+        char          mStoragePath[aos::cFilePathLen];
+        char          mStatePath[aos::cFilePathLen];
+    };
+
+    struct VersionInfo {
+        uint64_t mAosVersion;
+        char     mVendorVersion[aos::cVendorVersionLen];
+        char     mDescription[aos::cDescriptionLen];
+    };
+
+    struct ServiceData {
+        VersionInfo mVersionInfo;
+        char        mServiceID[aos::cServiceIDLen];
+        char        mProviderID[aos::cProviderIDLen];
+        char        mImagePath[aos::cFilePathLen];
+    };
+
+    InstanceInfo                         ConvertInstanceInfo(const aos::InstanceInfo& instance);
+    aos::InstanceInfo                    ConvertInstanceInfo(const InstanceInfo& instance);
+    ServiceData                          ConvertServiceData(const aos::sm::servicemanager::ServiceData& service);
+    aos::sm::servicemanager::ServiceData ConvertServiceData(const ServiceData& service);
+
+    FileStorage<InstanceInfo> mInstanceDatabase;
+    FileStorage<ServiceData>  mServiceDatabase;
+    aos::Mutex                mMutex;
 };
 
 #endif
