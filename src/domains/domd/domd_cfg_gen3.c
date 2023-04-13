504a782505f56d57bb8327ffecc708afcc608f39
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2023-04-13 20:52:11 +0300

    [domains] Move domain_creation routine to the separate header
    It appears that domains configuration is not common for rcar, qemu and
    native builds. We need to provide common api for all configurations,
    such as:
    - for RCAR boards - domd should be started.
    - for QEMU no need to start domains on init, but allocate dummy
    domd_cfg to avoid compilation errors in xen_cmds.
    - don't call create_domains for NATIVE configuration.
    
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- src/domains/domd/domd_cfg_gen3.c
+++ src/domains/domd/domd_cfg_gen3.c
@@ -6,7 +6,10 @@
 
 #include <domain.h>
 #include <string.h>
+#include <xen_dom_mgmt.h>
 #include <zephyr/xen/public/domctl.h>
+
+#include <domains/dom_runner.h>
 
 static char* domd_dtdevs[] = {
     "/soc/ethernet@e6800000",
@@ -539,4 +542,9 @@ struct xen_domain_cfg domd_cfg = {
 
     .dtb_start = __dtb_ipl_start,
     .dtb_end = __dtb_ipl_end,
+};
+
+int create_domains(void)
+{
+    return domain_create(&domd_cfg, 1);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,9 @@
 #include "logger/logger.hpp"
 #include "version.hpp"
 
-#if defined(CONFIG_SOC_SERIES_RCAR_GEN4) || defined(CONFIG_SOC_SERIES_RCAR_GEN3)
-extern "C" {
-#include <xen_dom_mgmt.h>
-
-extern struct xen_domain_cfg domd_cfg;
-}
-#elif !defined(CONFIG_NATIVE_APPLICATION)
-/* NOTE: add empty domd_cfg function because it's used in xen_cmds to prevent breaking the build */
-#include <domain.h>
-struct xen_domain_cfg domd_cfg;
-#endif /* CONFIG_SOC_SERIES_RCAR_GEN4 || CONFIG_SOC_SERIES_RCAR_GEN3 */
+#if !defined(CONFIG_NATIVE_APPLICATION)
+#include <domains/dom_runner.h>
+#endif
 
 int main(void)
 {
@@ -33,12 +25,12 @@ int main(void)
     printk("*** Aos core library: %s ***\n", AOS_CORE_VERSION);
     printk("*** Aos core size: %lu ***\n", sizeof(App));
 
-#if defined(CONFIG_SOC_SERIES_RCAR_GEN4) || defined(CONFIG_SOC_SERIES_RCAR_GEN3)
-    int rc = domain_create(&domd_cfg, 1);
+#if !defined(CONFIG_NATIVE_APPLICATION)
+    auto rc = create_domains();
     if (rc) {
         printk("failed to create domain (%d)\n", rc);
     }
-#endif /* CONFIG_SOC_SERIES_RCAR_GEN4 || CONFIG_SOC_SERIES_RCAR_GEN3 */
+#endif
 
     Logger::Init();
 
