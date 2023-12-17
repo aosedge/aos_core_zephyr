045d491974871bbb852f4fab2e44648c70db80d7
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2023-12-17 22:28:41 +0200

    [resourcemanager] Add ResourceManagerItf to resource manager
    We need to add interface in order to properly implement uint tests.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/resourcemanager/resourcemanager.hpp
+++ src/resourcemanager/resourcemanager.hpp
@@ -12,18 +12,23 @@
 #include <aos/common/tools/noncopyable.hpp>
 
 /**
- * Resource manager instance.
+ * Resource manager interface.
  */
 
-class ResourceManager : public aos::NonCopyable {
+class ResourceManagerItf {
 public:
+    /**
+     * Destructor
+     */
+    virtual ~ResourceManagerItf() = default;
+
     /**
      * Get current unit config version.
      *
      * @param version  [out] param to store version.
      * @return aos::Error.
      */
-    aos::Error GetUnitConfigInfo(char* version) const;
+    virtual aos::Error GetUnitConfigInfo(char* version) const = 0;
 
     /**
      * Check new unit configuration.
@@ -32,7 +37,7 @@ public:
      * @param unitConfig string with unit configuration.
      * @return aos::Error.
      */
-    aos::Error CheckUnitConfig(const char* version, const char* unitConfig) const;
+    virtual aos::Error CheckUnitConfig(const char* version, const char* unitConfig) const = 0;
 
     /**
      * Update unit configuration.
@@ -41,7 +46,40 @@ public:
      * @param unitConfig string with unit configuration.
      * @return aos::Error.
      */
-    aos::Error UpdateUnitConfig(const char* version, const char* unitConfig);
+    virtual aos::Error UpdateUnitConfig(const char* version, const char* unitConfig) = 0;
+};
+
+/**
+ * Resource manager instance.
+ */
+
+class ResourceManager : public ResourceManagerItf, private aos::NonCopyable {
+public:
+    /**
+     * Get current unit config version.
+     *
+     * @param version  [out] param to store version.
+     * @return aos::Error.
+     */
+    aos::Error GetUnitConfigInfo(char* version) const override;
+
+    /**
+     * Check new unit configuration.
+     *
+     * @param version unit config version
+     * @param unitConfig string with unit configuration.
+     * @return aos::Error.
+     */
+    aos::Error CheckUnitConfig(const char* version, const char* unitConfig) const override;
+
+    /**
+     * Update unit configuration.
+     *
+     * @param version unit config version.
+     * @param unitConfig string with unit configuration.
+     * @return aos::Error.
+     */
+    aos::Error UpdateUnitConfig(const char* version, const char* unitConfig) override;
 
 private:
     static constexpr size_t cVersionLen = 10;
