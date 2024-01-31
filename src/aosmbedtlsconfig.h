035b5fcd118e94c80e722d531741ad1418c5803c
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-01-31 11:29:28 +0200

    [mbedtls] Integrate builtin mbedtls with aos core
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/aosmbedtlsconfig.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOSMBEDTLSCONFIG_H_
+#define AOSMBEDTLSCONFIG_H_
+
+#define MBEDTLS_NO_PLATFORM_ENTROPY
+#define MBEDTLS_ENTROPY_HARDWARE_ALT
+
+#include "../../aos_core_lib_cpp/include/aos/common/crypto/mbedtls/mbedtls_config.h"
+
+#endif
