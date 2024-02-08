d07a3a5d6fab8880d27a234de0feeaa1afac4b12
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-02-08 22:13:13 +0200

    [communication] Add retry read/write for vchannel
    Regardless, vchanall is opened in blocking mode, it is observed that it can't
    write/read big packets at once. Add retry mechanism for such cases.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/communication/vchannel.cpp
+++ src/communication/vchannel.cpp
@@ -53,22 +53,25 @@ aos::Error VChannel::Close()
 
 aos::Error VChannel::Read(aos::Array<uint8_t>& data, size_t size)
 {
+    size_t read = 0;
+
     auto err = data.Resize(size);
     if (!err.IsNone()) {
         return AOS_ERROR_WRAP(err);
     }
 
-    auto ret = vch_read(&mReadHandle, data.Get(), size);
-    if (ret < 0) {
-        return AOS_ERROR_WRAP(ret);
-    }
-
-    if (static_cast<size_t>(ret) != size) {
-        err = data.Resize(ret);
-        if (!err.IsNone()) {
-            return AOS_ERROR_WRAP(err);
+    while (read < size) {
+        auto ret = vch_read(&mReadHandle, data.Get() + read, size - read);
+        if (ret < 0) {
+            return AOS_ERROR_WRAP(ret);
         }
 
+        read += ret;
+    }
+
+    assert(read <= data.MaxSize());
+
+    if (read != size) {
         return AOS_ERROR_WRAP(aos::ErrorEnum::eFailed);
     }
 
@@ -77,12 +80,18 @@ aos::Error VChannel::Read(aos::Array<uint8_t>& data, size_t size)
 
 aos::Error VChannel::Write(const aos::Array<uint8_t>& data)
 {
-    auto ret = vch_write(&mWriteHandle, data.Get(), data.Size());
-    if (ret < 0) {
-        return AOS_ERROR_WRAP(ret);
+    size_t written = 0;
+
+    while (written < data.Size()) {
+        auto ret = vch_write(&mWriteHandle, data.Get() + written, data.Size() - written);
+        if (ret < 0) {
+            return AOS_ERROR_WRAP(ret);
+        }
+
+        written += ret;
     }
 
-    if (static_cast<size_t>(ret) != data.Size()) {
+    if (written != data.Size()) {
         return AOS_ERROR_WRAP(aos::ErrorEnum::eFailed);
     }
 
