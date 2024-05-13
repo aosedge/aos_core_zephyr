2918941da994f3bd3d19c3677abfac865fc443b6
Dmytro Firsov <dmytro_firsov@epam.com>
2024-05-13 10:31:34 +0300

    domains: refactor domain creation logic
    Application needs to have some interface for domain creation.
    Previously it was single call create_domains() that created only
    Domain-D by calling bunch of function in board files which is not very
    convinient. Also it was hard to extend it with one more domain
    (Domain-U), that will be introduced soon.
    
    Now create_domains() is located in single file and call zephyr-xenlib
    directly as the logic is same for all boards. It will use dedicated
    domain config included on build time.
    
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Reviewed-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

---
+++ src/domains/dom_runner.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <domain.h>
+#include <xen_dom_mgmt.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(domains);
+
+/* This is needed until we will not use domain names for backend confs */
+#define DOMID_DOMD 1
+
+extern struct xen_domain_cfg domd_cfg;
+
+int create_domains(void)
+{
+    int rc;
+
+    rc = domain_create(&domd_cfg, DOMID_DOMD);
+    if (rc < 0) {
+        LOG_ERR("Failed to start Domain-D, rc = %d", rc);
+        return rc;
+    } else if (rc != DOMID_DOMD) {
+        /* Since this value is used for backend configs, it is critical error for us */
+        LOG_ERR("Failed to start Domain-D with specified domid");
+        return rc;
+    }
+
+    return 0;
+}
