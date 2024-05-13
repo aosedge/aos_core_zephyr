d47fcea6c1dfa68eafdf270d4f6b07d14f100a62
Dmytro Firsov <dmytro_firsov@epam.com>
2024-05-13 11:27:09 +0300

    domains: introduce Domain-U creation
    Add domain configuration and creation logic for Domain-U in
    AoS application. It is controlled by previously introduced option
    CONFIG_DOMU_ENABLE and supports backend configuration for network
    and pvblock, which are currently assigned to domain. Domain-U uses
    partition on UFS as rootfs, so it is needed to create proper image
    during build.
    
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>
    Reviewed-by: Grygorii Strashko <grygorii_strashko@epam.com>
    Acked-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

---
+++ src/domains/domu/domu_runner.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2024 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef DOMAIN_U_RUNNER_H_
+#define DOMAIN_U_RUNNER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int domu_start(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* DOMAIN_U_RUNNER_H_ */
