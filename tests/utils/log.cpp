4a009032a9c1b40ab253bee80f7c3608b930e514
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-08-05 21:56:33 +0300

    [clocksync] Implement clock sync notification subscriber
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ tests/utils/log.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <mutex>
+
+#include <zephyr/sys/printk.h>
+
+#include "log.hpp"
+
+void TestLogCallback(const char* module, aos::LogLevel level, const aos::String& message)
+{
+    static std::mutex mutex;
+    static auto       startTime = std::chrono::steady_clock::now();
+
+    std::lock_guard<std::mutex> lock {mutex};
+
+    auto now = std::chrono::duration<float>(std::chrono::steady_clock::now() - startTime).count();
+
+    const char* levelStr = "unknown";
+
+    switch (level.GetValue()) {
+    case aos::LogLevelEnum::eDebug:
+        levelStr = "dbg";
+        break;
+
+    case aos::LogLevelEnum::eInfo:
+        levelStr = "inf";
+        break;
+
+    case aos::LogLevelEnum::eWarning:
+        levelStr = "wrn";
+        break;
+
+    case aos::LogLevelEnum::eError:
+        levelStr = "err";
+        break;
+
+    default:
+        levelStr = "n/d";
+        break;
+    }
+
+    printk("%0.3f (%s) [%s] %s\n", now, module, levelStr, message.CStr());
+}
