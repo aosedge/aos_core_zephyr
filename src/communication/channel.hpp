581e0ca3f0d65a45f489dc17188b8a31bd7c5812
Mykola Solianko <mykola_solianko@epam.com>
2024-08-06 12:08:57 +0300

    [communication] Implement channel
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/communication/channel.hpp
+++ src/communication/channel.hpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2023 Renesas Electronics Corporation.
- * Copyright (C) 2023 EPAM Systems, Inc.
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,9 +8,13 @@
 #ifndef CHANNEL_HPP_
 #define CHANNEL_HPP_
 
-namespace aos::zephyr::communication {
-
 #include <aos/common/tools/error.hpp>
+#include <aos/common/tools/thread.hpp>
+#include <aosprotocol.h>
+
+#include "transport.hpp"
+
+namespace aos::zephyr::communication {
 
 /**
  * Channel interface.
@@ -22,12 +26,12 @@ public:
      *
      * @return aos::Error.
      */
-    virtual aos::Error Connect() = 0;
+    virtual Error Connect() = 0;
 
     /**
      * Closes current connection.
      */
-    virtual aos::Error Close() = 0;
+    virtual Error Close() = 0;
 
     /**
      * Returns if channel is connected.
@@ -58,6 +62,117 @@ public:
      * Destructor.
      */
     virtual ~ChannelItf() = default;
+};
+
+/**
+ * Communication interface.
+ */
+class CommunicationItf {
+public:
+    /**
+     * Connects to communication channel.
+     *
+     * @return aos::Error.
+     */
+    virtual Error Connect() = 0;
+
+    /**
+     * Closes current connection.
+     *
+     * @return aos::Error.
+     */
+    virtual bool IsConnected() const = 0;
+
+    /**
+     * Reads data from channel to array.
+     *
+     * @param data buffer where data is placed to.
+     * @param size specifies how many bytes to read.
+     * @return int num read bytes.
+     */
+    virtual int Write(uint32_t port, const void* data, size_t size) = 0;
+};
+
+/**
+ * Channel class.
+ */
+class Channel : public ChannelItf {
+public:
+    /**
+     * Constructor.
+     *
+     * @param communication communication interface.
+     * @param port port number.
+     */
+    Channel(CommunicationItf* communication, int port);
+
+    /**
+     * Connects to communication channel.
+     *
+     * @return aos::Error.
+     */
+    Error Connect() override;
+
+    /**
+     * Closes current connection.
+     *
+     * @return aos::Error.
+     */
+    Error Close() override;
+
+    /**
+     * Reads data from channel to buffer.
+     *
+     * @param data buffer where data is placed to.
+     * @param size specifies how many bytes to read.
+     * @return int num read bytes.
+     */
+    int Read(void* buffer, size_t size) override;
+
+    /**
+     * Writes data from buffer to channel.
+     *
+     * @param data data buffer.
+     * @param size specifies how many bytes to write.
+     * @return int num written bytes.
+     */
+    int Write(const void* buffer, size_t size) override;
+
+    /**
+     * Returns if channel is connected.
+     *
+     * @return bool.
+     */
+    bool IsConnected() const override;
+
+    /**
+     * Waits for read.
+     *
+     * @param buffer buffer where data is placed to.
+     * @param size specifies how many bytes to read.
+     * @return aos::Error.
+     */
+    Error WaitRead(void** buffer, size_t* size);
+
+    /**
+     * Notifies that read is done.
+     *
+     * @param size specifies how many bytes were read.
+     * @return aos::Error.
+     */
+    Error ReadDone(size_t size);
+
+private:
+    static constexpr auto cWaitReadPeriod = 3 * Time::cSeconds;
+
+    CommunicationItf*   mCommunication {};
+    int                 mPort {};
+    bool                mReadReady {};
+    bool                mClose {};
+    uint8_t*            mBuffer {};
+    size_t              mBufferLen {};
+    Mutex               mMutex;
+    ConditionalVariable mCondVar;
 };
 
 } // namespace aos::zephyr::communication
