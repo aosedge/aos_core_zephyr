42692850c79a15ed4acd6e7f25b3224d0fabb4f7
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-08-17 17:56:36 +0300

    [provisionmanager] Add initial provision manager callback implementation
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/provisionmanager/log.hpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2023 Renesas Electronics Corporation.
+ * Copyright (C) 2023 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LOG_HPP_
+#define LOG_HPP_
+
+#define LOG_MODULE "provisionmanager"
+
+#include <aos/common/tools/logger.hpp>
+
+#endif
