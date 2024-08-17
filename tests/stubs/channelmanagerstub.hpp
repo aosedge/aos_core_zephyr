3077de124052dbcd75093c0acccfb4f13df24560
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-08-17 16:36:52 +0300

    [iamclient] Implement switching channels on provision/deprovision
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- tests/stubs/channelmanagerstub.hpp
+++ tests/stubs/channelmanagerstub.hpp
@@ -10,12 +10,16 @@
 
 #include <unordered_map>
 
+#include "communication/channelmanager.hpp"
+
 #include "channelstub.hpp"
 
 class ChannelManagerStub : public aos::zephyr::communication::ChannelManagerItf {
 public:
     aos::RetWithError<aos::zephyr::communication::ChannelItf*> CreateChannel(uint32_t port) override
     {
+        std::lock_guard lock {mMutex};
+
         if (mChannels.find(port) != mChannels.end()) {
             return {nullptr, aos::ErrorEnum::eAlreadyExist};
         }
@@ -24,23 +28,32 @@ public:
 
         mChannels[port] = std::move(channel);
 
+        mCV.notify_one();
+
         return mChannels[port].get();
     }
 
     aos::Error DeleteChannel(uint32_t port) override
     {
+        std::lock_guard lock {mMutex};
+
         if (mChannels.find(port) == mChannels.end()) {
             return aos::ErrorEnum::eNotFound;
         }
 
         mChannels.erase(port);
 
+        mCV.notify_one();
+
         return aos::ErrorEnum::eNone;
     }
 
-    aos::RetWithError<ChannelStub*> GetChannel(uint32_t port)
+    aos::RetWithError<ChannelStub*> GetChannel(
+        uint32_t port, const std::chrono::duration<double>& timeout = std::chrono::duration<double>::zero())
     {
-        if (mChannels.find(port) == mChannels.end()) {
+        std::unique_lock lock {mMutex};
+
+        if (!mCV.wait_for(lock, timeout, [&] { return mChannels.find(port) != mChannels.end(); })) {
             return {nullptr, aos::ErrorEnum::eNotFound};
         }
 
@@ -49,6 +62,8 @@ public:
 
 private:
     std::unordered_map<uint32_t, std::unique_ptr<ChannelStub>> mChannels;
+    std::mutex                                                 mMutex;
+    std::condition_variable                                    mCV;
 };
 
 #endif
