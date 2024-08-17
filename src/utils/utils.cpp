4fbe83db68a9c79d3dbbb1d5f06fa532ef58dc38
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-08-17 17:42:21 +0300

    [utils] Combine test utils with src utils and update API
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -6,22 +6,30 @@
  */
 
 #include <aos/common/tools/string.hpp>
+#include <aos/common/tools/thread.hpp>
 
 #include "utils.hpp"
+
+namespace aos::zephyr::utils {
 
 /***********************************************************************************************************************
  * Static
  **********************************************************************************************************************/
 
-aos::StaticString<256> sErrorStr;
+static StaticString<256> sErrorStr;
+static Mutex             sMutex;
 
 /***********************************************************************************************************************
  * Public
  **********************************************************************************************************************/
 
-const char* AosErrorToStr(const aos::Error& err)
+const char* ErrorToCStr(const Error& err)
 {
+    LockGuard lock {sMutex};
+
     sErrorStr.Convert(err);
 
     return sErrorStr.CStr();
 }
+
+} // namespace aos::zephyr::utils
---
+++ src/utils/utils.hpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _TEST_UTILS_HPP
+#define _TEST_UTILS_HPP
+
+#include <aos/common/tools/error.hpp>
+
+namespace aos::zephyr::utils {
+
+/**
+ * Creates Aos string from fixed size C string.
+ *
+ * @tparam cSize max C string size.
+ * @param cStr C string.
+ * @return String&.
+ */
+template <size_t cSize>
+constexpr String StringFromCStr(char (&cStr)[cSize])
+{
+    return String(cStr, cSize - 1);
+}
+
+/**
+ * Creates Aos string from fixed size C string.
+ *
+ * @tparam cSize max C string size.
+ * @param cStr C string.
+ * @return String&.
+ */
+template <size_t cSize>
+constexpr const String StringFromCStr(const char (&cStr)[cSize])
+{
+    return String(cStr, cSize - 1);
+}
+
+/**
+ * Creates Aos array from fixed size C array.
+ *
+ * @tparam T C array type.
+ * @tparam cSize max C array size.
+ * @param arr C array.
+ * @return Array<T>.
+ */
+template <typename T, size_t cSize>
+Array<T> ToArray(T (&arr)[cSize])
+{
+    return Array<T>(arr, cSize);
+}
+
+/**
+ * Creates Aos array from fixed size C array.
+ *
+ * @tparam T C array type.
+ * @tparam cSize max C array size.
+ * @param arr C array.
+ * @return Array<T>.
+ */
+template <typename T, size_t cSize>
+const Array<T> ToArray(const T (&arr)[cSize])
+{
+    return Array<T>(arr, cSize);
+}
+
+/**
+ * Converts Aos error to string.
+ *
+ * @param err aos error.
+ * @return const char*
+ */
+const char* ErrorToCStr(const Error& err);
+
+} // namespace aos::zephyr::utils
+
+#endif
