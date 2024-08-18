1f58c943423659e480e9e2d5c9419524261fcd31
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-08-18 15:41:01 +0300

    [nodeinfoprovider] Use machine-id as node id and host fs as aos storage
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- tests/nodeinfoprovider/src/main.cpp
+++ tests/nodeinfoprovider/src/main.cpp
@@ -5,8 +5,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/fs/fs.h>
-#include <zephyr/fs/littlefs.h>
+#include <sys/statvfs.h>
+
+#include <zephyr/tc_util.h>
 #include <zephyr/ztest.h>
 
 #include <aos/common/tools/fs.hpp>
@@ -16,11 +17,9 @@
 
 #include "nodeinfoprovider/nodeinfoprovider.hpp"
 #include "utils/log.hpp"
+#include "utils/utils.hpp"
 
-#define LVGL_PARTITION    storage_partition
-#define LVGL_PARTITION_ID FIXED_PARTITION_ID(LVGL_PARTITION)
-
-FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(cstorage);
+using namespace aos::zephyr;
 
 /***********************************************************************************************************************
  * Consts
@@ -33,62 +32,26 @@ static constexpr auto cUnprovisioned = "unprovisioned";
  * Static
  **********************************************************************************************************************/
 
-static struct fs_mount_t sMnt = {
-    .type        = FS_LITTLEFS,
-    .mnt_point   = CONFIG_AOS_DISK_MOUNT_POINT,
-    .fs_data     = &cstorage,
-    .storage_dev = (void*)LVGL_PARTITION_ID,
-};
-
-static struct fs_statvfs sMntStat;
-
-void* setup_fs(void)
+void* setup(void)
 {
-    printk("setup_fs\n");
+    aos::Log::SetCallback(TestLogCallback);
 
-    int ret = fs_mount(&sMnt);
-    if (ret < 0) {
-        TC_PRINT("Failed to mount file system: %d\n", ret);
-
-        ztest_test_fail();
-
-        return NULL;
-    }
-
-    ret = fs_statvfs(CONFIG_AOS_DISK_MOUNT_POINT, &sMntStat);
-    if (ret != 0) {
-        printk("Failed to get statvfs: %d\n", ret);
-    } else {
-        printk("Mounted partition: %s\n", CONFIG_AOS_DISK_MOUNT_POINT);
-        printk("bsize = %lu\n", sMntStat.f_bsize);
-        printk("frsize = %lu\n", sMntStat.f_frsize);
-        printk("blocks = %lu\n", sMntStat.f_blocks);
-        printk("bfree = %lu\n", sMntStat.f_bfree);
-    }
+    auto err = aos::FS::ClearDir(CONFIG_AOS_DISK_MOUNT_POINT);
+    zassert_true(err.IsNone(), "Failed to clear disk mount point: %s", utils::ErrorToCStr(err));
 
     return NULL;
 }
 
-void teardown_fs(void* data)
+void teardown(void* data)
 {
-    printk("teardown_fs\n");
-
-    auto ret = fs_unmount(&sMnt);
-    if (ret < 0) {
-        printk("Failed to unmount file system: %d\n", ret);
-    }
-
-    return;
+    auto err = aos::FS::RemoveAll(CONFIG_AOS_DISK_MOUNT_POINT);
+    zassert_true(err.IsNone(), "Failed to remove disk mount point: %s", utils::ErrorToCStr(err));
 }
 
 void before_test(void* data)
 {
-    printk("before_test\n");
-
     auto err = aos::FS::WriteStringToFile(cNodeStateFile, cUnprovisioned, S_IRUSR | S_IWUSR);
-    if (!err.IsNone()) {
-        printk("Can't create provisioning state file: %s\n", err.Message());
-    }
+    zassert_true(err.IsNone(), "Failed to create provisioning state file: %s", utils::ErrorToCStr(err));
 }
 
 /***********************************************************************************************************************
@@ -99,8 +62,6 @@ class TestNodeStatusObserver : public aos::iam::nodeinfoprovider::NodeStatusObse
 public:
     aos::Error OnNodeStatusChanged(const aos::String& nodeID, const aos::NodeStatus& status) override
     {
-        printk("Node status changed: %s\n", status.ToString().CStr());
-
         mNodeID     = nodeID;
         mNodeStatus = status;
         mNotified   = true;
@@ -124,7 +85,7 @@ public:
  * Setup
  **********************************************************************************************************************/
 
-ZTEST_SUITE(nodeinfoprovider, NULL, setup_fs, before_test, NULL, teardown_fs);
+ZTEST_SUITE(nodeinfoprovider, nullptr, setup, before_test, nullptr, teardown);
 
 /***********************************************************************************************************************
  * Tests
@@ -132,26 +93,30 @@ ZTEST_SUITE(nodeinfoprovider, NULL, setup_fs, before_test, NULL, teardown_fs);
 
 ZTEST(nodeinfoprovider, test_node_info)
 {
-    aos::Log::SetCallback(TestLogCallback);
-
     aos::zephyr::nodeinfoprovider::NodeInfoProvider provider;
-    auto                                            err = provider.Init();
-    zassert_true(err.IsNone(), "Failed to init node info provider: %s", err.Message());
+
+    auto err = provider.Init();
+    zassert_true(err.IsNone(), "Failed to init node info provider: %s", utils::ErrorToCStr(err));
 
     aos::NodeInfo nodeInfo;
     err = provider.GetNodeInfo(nodeInfo);
 
-    zassert_true(err.IsNone(), "Failed to get node info: %s", err.Message());
+    zassert_true(err.IsNone(), "Failed to get node info: %s", utils::ErrorToCStr(err));
 
     zassert_equal(nodeInfo.mStatus.GetValue(), aos::NodeStatusEnum::eUnprovisioned, "Node status mismatch");
     zassert_equal(nodeInfo.mMaxDMIPS, CONFIG_AOS_MAX_CPU_DMIPS, "Max DMIPS mismatch");
 
     // test partition info
     zassert_equal(nodeInfo.mPartitions.Size(), 1, "Expected 1 partition");
-    zassert_equal(nodeInfo.mPartitions[0].mName, "Aos", "Partition name mismatch");
+    zassert_equal(nodeInfo.mPartitions[0].mName, "aos", "Partition name mismatch");
     zassert_equal(nodeInfo.mPartitions[0].mPath, CONFIG_AOS_DISK_MOUNT_POINT, "Partition path mismatch");
 
-    const auto expectedSize = sMntStat.f_bsize * sMntStat.f_blocks;
+    struct statvfs sbuf;
+
+    auto ret = statvfs(CONFIG_AOS_DISK_MOUNT_POINT, &sbuf);
+    zassert_equal(ret, 0, "Failed to get partition info: %s [%d]", strerror(ret), ret);
+
+    const auto expectedSize = sbuf.f_bsize * sbuf.f_blocks;
     zassert_equal(nodeInfo.mPartitions[0].mTotalSize, expectedSize, "Partition total size mismatch");
 
     zassert_equal(nodeInfo.mPartitions[0].mTypes.Size(), 2, "Expected 2 partition types");
@@ -169,59 +134,55 @@ ZTEST(nodeinfoprovider, test_node_info)
 
 ZTEST(nodeinfoprovider, test_set_get_node_info)
 {
-    aos::Log::SetCallback(TestLogCallback);
-
     aos::zephyr::nodeinfoprovider::NodeInfoProvider provider;
     auto                                            err = provider.Init();
-    zassert_true(err.IsNone(), "Failed to init node info provider: %s", err.Message());
+    zassert_true(err.IsNone(), "Failed to init node info provider: %s", utils::ErrorToCStr(err));
 
     aos::NodeInfo nodeInfo;
     err = provider.GetNodeInfo(nodeInfo);
 
-    zassert_true(err.IsNone(), "Failed to get node info: %s", err.Message());
+    zassert_true(err.IsNone(), "Failed to get node info: %s", utils::ErrorToCStr(err));
     zassert_equal(nodeInfo.mStatus.GetValue(), aos::NodeStatusEnum::eUnprovisioned, "Node status mismatch");
 
     err = provider.SetNodeStatus(aos::NodeStatus(aos::NodeStatusEnum::eProvisioned));
-    zassert_true(err.IsNone(), "Set node status failed: %s", err.Message());
+    zassert_true(err.IsNone(), "Set node status failed: %s", utils::ErrorToCStr(err));
 
     err = provider.GetNodeInfo(nodeInfo);
-    zassert_true(err.IsNone(), "Get node status failed: %s", err.Message());
+    zassert_true(err.IsNone(), "Get node status failed: %s", utils::ErrorToCStr(err));
 
     zassert_equal(nodeInfo.mStatus.GetValue(), aos::NodeStatusEnum::eProvisioned, "Node status mismatch");
 
     err = provider.SetNodeStatus(aos::NodeStatus(aos::NodeStatusEnum::ePaused));
-    zassert_true(err.IsNone(), "Set node status failed: %s", err.Message());
+    zassert_true(err.IsNone(), "Set node status failed: %s", utils::ErrorToCStr(err));
 
     err = provider.GetNodeInfo(nodeInfo);
-    zassert_true(err.IsNone(), "Get node status failed: %s", err.Message());
+    zassert_true(err.IsNone(), "Get node status failed: %s", utils::ErrorToCStr(err));
 
     zassert_equal(nodeInfo.mStatus.GetValue(), aos::NodeStatusEnum::ePaused, "Node status mismatch");
 }
 
 ZTEST(nodeinfoprovider, test_node_status_changed_observer)
 {
-    aos::Log::SetCallback(TestLogCallback);
-
     aos::zephyr::nodeinfoprovider::NodeInfoProvider provider;
     auto                                            err = provider.Init();
-    zassert_true(err.IsNone(), "Failed to init node info provider: %s", err.Message());
+    zassert_true(err.IsNone(), "Failed to init node info provider: %s", utils::ErrorToCStr(err));
 
     aos::NodeInfo nodeInfo;
     err = provider.GetNodeInfo(nodeInfo);
 
-    zassert_true(err.IsNone(), "Failed to get node info: %s", err.Message());
+    zassert_true(err.IsNone(), "Failed to get node info: %s", utils::ErrorToCStr(err));
     zassert_equal(nodeInfo.mStatus.GetValue(), aos::NodeStatusEnum::eUnprovisioned, "Node status mismatch");
 
     TestNodeStatusObserver observer1, observer2;
 
     err = provider.SubscribeNodeStatusChanged(observer1);
-    zassert_true(err.IsNone(), "Failed to subscribe on node status changed: %s", err.Message());
+    zassert_true(err.IsNone(), "Failed to subscribe on node status changed: %s", utils::ErrorToCStr(err));
 
     err = provider.SubscribeNodeStatusChanged(observer2);
-    zassert_true(err.IsNone(), "Failed to subscribe on node status changed: %s", err.Message());
+    zassert_true(err.IsNone(), "Failed to subscribe on node status changed: %s", utils::ErrorToCStr(err));
 
     err = provider.SetNodeStatus(aos::NodeStatus(aos::NodeStatusEnum::eProvisioned));
-    zassert_true(err.IsNone(), "Set node status failed: %s", err.Message());
+    zassert_true(err.IsNone(), "Set node status failed: %s", utils::ErrorToCStr(err));
 
     zassert_equal(observer1.mNodeStatus.GetValue(), aos::NodeStatusEnum::eProvisioned, "Node status mismatch");
     zassert_equal(observer1.mNodeID, nodeInfo.mNodeID, "Node id mismatch");
@@ -233,7 +194,7 @@ ZTEST(nodeinfoprovider, test_node_status_changed_observer)
     observer2.Reset();
 
     err = provider.SetNodeStatus(aos::NodeStatus(aos::NodeStatusEnum::ePaused));
-    zassert_true(err.IsNone(), "Set node status failed: %s", err.Message());
+    zassert_true(err.IsNone(), "Set node status failed: %s", utils::ErrorToCStr(err));
 
     zassert_equal(observer1.mNodeStatus.GetValue(), aos::NodeStatusEnum::ePaused, "Node status mismatch");
     zassert_equal(observer1.mNodeID, nodeInfo.mNodeID, "Node id mismatch");
@@ -246,7 +207,7 @@ ZTEST(nodeinfoprovider, test_node_status_changed_observer)
 
     // same node status change should not trigger the observer
     err = provider.SetNodeStatus(aos::NodeStatus(aos::NodeStatusEnum::ePaused));
-    zassert_true(err.IsNone(), "Set node status failed: %s", err.Message());
+    zassert_true(err.IsNone(), "Set node status failed: %s", utils::ErrorToCStr(err));
 
     zassert_false(observer1.mNotified, "Observer should not be notified");
     zassert_false(observer2.mNotified, "Observer should not be notified");
@@ -256,10 +217,10 @@ ZTEST(nodeinfoprovider, test_node_status_changed_observer)
 
     // unsubscribe observer1
     err = provider.UnsubscribeNodeStatusChanged(observer1);
-    zassert_true(err.IsNone(), "Failed to unsubscribe from node status changed event: %s", err.Message());
+    zassert_true(err.IsNone(), "Failed to unsubscribe from node status changed event: %s", utils::ErrorToCStr(err));
 
     err = provider.SetNodeStatus(aos::NodeStatus(aos::NodeStatusEnum::eProvisioned));
-    zassert_true(err.IsNone(), "Set node status failed: %s", err.Message());
+    zassert_true(err.IsNone(), "Set node status failed: %s", utils::ErrorToCStr(err));
 
     // observer1 should not receive the event
     zassert_false(observer1.mNotified, "Observer should not be notified");
