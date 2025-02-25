cac818d3e641231917694ec07b4b3f38fd367052
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-02-25 15:56:09 +0200

    [resourcemanager] Sync interfaces with core lib
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- src/resourcemanager/resourcemanager.hpp
+++ src/resourcemanager/resourcemanager.hpp
@@ -23,20 +23,20 @@ public:
     /**
      * Dumps config object into string.
      *
-     * @param config config object.
-     * @param[out] json json representation of config.
+     * @param nodeConfig node config object.
+     * @param[out] json node config JSON string.
      * @return Error.
      */
-    Error DumpNodeConfig(const sm::resourcemanager::NodeConfig& config, String& json) const override;
+    Error NodeConfigToJSON(const sm::resourcemanager::NodeConfig& nodeConfig, String& json) const override;
 
     /**
-     * Parses config object from string.
+     * Creates node config object from a JSON string.
      *
-     * @param json json representation of config.
-     * @param[out] config config.
+     * @param json node config JSON string.
+     * @param[out] nodeConfig node config object.
      * @return Error.
      */
-    Error ParseNodeConfig(const String& json, sm::resourcemanager::NodeConfig& config) const override;
+    Error NodeConfigFromJSON(const String& json, sm::resourcemanager::NodeConfig& nodeConfig) const override;
 
 private:
     mutable Mutex                                                      mMutex;
@@ -50,61 +50,20 @@ private:
 class HostDeviceManager : public sm::resourcemanager::HostDeviceManagerItf {
 public:
     /**
-     * Allocates device for instance.
-     *
-     * @param deviceInfo device info.
-     * @param instanceID instance ID.
-     * @return Error.
-     */
-    Error AllocateDevice(const DeviceInfo& deviceInfo, const String& instanceID) override;
-
-    /**
-     * Removes instance from device.
-     *
-     * @param deviceName device name.
-     * @param instanceID instance ID.
-     * @return Error.
-     */
-    Error RemoveInstanceFromDevice(const String& deviceName, const String& instanceID) override;
-
-    /**
-     * Removes instance from all devices.
-     *
-     * @param instanceID instance ID.
-     * @return Error.
-     */
-    Error RemoveInstanceFromAllDevices(const String& instanceID) override;
-
-    /**
-     * Returns ID list of instances that allocate specific device.
-     *
-     * @param deviceName device name.
-     * @param instances[out] param to store instance ID(s).
-     * @return Error.
-     */
-    Error GetDeviceInstances(const String& deviceName, Array<StaticString<cInstanceIDLen>>& instanceIDs) const override;
-
-    /**
      * Checks if device exists.
      *
      * @param device device name.
      * @return true if device exists, false otherwise.
      */
-    bool DeviceExists(const String& device) const override;
-};
+    Error CheckDevice(const String& device) const override;
 
-/**
- * Host group manager.
- */
-class HostGroupManager : public sm::resourcemanager::HostGroupManagerItf {
-public:
     /**
      * Checks if group exists.
      *
      * @param group group name.
      * @return true if group exists, false otherwise.
      */
-    bool GroupExists(const String& group) const override;
+    Error CheckGroup(const String& group) const override;
 };
 
 } // namespace aos::zephyr::resourcemanager
