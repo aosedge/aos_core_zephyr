6a5e67d3df4615308b745fc6319f8ad1c862f6c5
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-02-28 13:05:13 +0200

    [tests,iamclient] Sync provision manager stub with core lib interface
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- tests/iamclient/src/stubs/provisionmanagerstub.hpp
+++ tests/iamclient/src/stubs/provisionmanagerstub.hpp
@@ -8,94 +8,78 @@
 #ifndef PROVISIONMANAGERSTUB_HPP_
 #define PROVISIONMANAGERSTUB_HPP_
 
+#include <aos/iam/certprovider.hpp>
 #include <aos/iam/provisionmanager.hpp>
 
-class ProvisionManagerStub : public aos::iam::provisionmanager::ProvisionManagerItf {
+namespace aos::zephyr {
+
+/**
+ * Provision manager stub.
+ */
+class ProvisionManagerStub : public iam::provisionmanager::ProvisionManagerItf {
 public:
-    aos::Error StartProvisioning(const aos::String& password) override
+    Error StartProvisioning(const String& password) override
     {
         mPassword = password;
 
-        return aos::ErrorEnum::eNone;
+        return ErrorEnum::eNone;
     }
 
-    aos::RetWithError<aos::iam::provisionmanager::CertTypes> GetCertTypes() const { return mCertTypes; }
+    RetWithError<iam::provisionmanager::CertTypes> GetCertTypes() const { return mCertTypes; }
 
-    aos::Error CreateKey(
-        const aos::String& certType, const aos::String& subject, const aos::String& password, aos::String& csr) override
+    Error CreateKey(const String& certType, const String& subject, const String& password, String& csr) override
     {
         mCertType = certType;
         mSubject  = subject;
         mPassword = password;
         csr       = mCSR;
 
-        return aos::ErrorEnum::eNone;
+        return ErrorEnum::eNone;
     }
 
-    aos::Error ApplyCert(
-        const aos::String& certType, const aos::String& pemCert, aos::iam::certhandler::CertInfo& certInfo) override
+    Error ApplyCert(const String& certType, const String& pemCert, iam::certhandler::CertInfo& certInfo) override
     {
         mCertType = certType;
         mCert     = pemCert;
         certInfo  = mCertInfo;
 
-        return aos::ErrorEnum::eNone;
+        return ErrorEnum::eNone;
     }
 
-    aos::Error GetCert(const aos::String& certType, const aos::Array<uint8_t>& issuer,
-        const aos::Array<uint8_t>& serial, aos::iam::certhandler::CertInfo& resCert) const override
-    {
-        return aos::ErrorEnum::eNone;
-    }
-
-    aos::Error SubscribeCertChanged(
-        const aos::String& certType, aos::iam::certhandler::CertReceiverItf& certReceiver) override
-    {
-        (void)certType;
-        (void)certReceiver;
-
-        return aos::ErrorEnum::eNone;
-    }
-
-    aos::Error UnsubscribeCertChanged(aos::iam::certhandler::CertReceiverItf& certReceiver) override
-    {
-        (void)certReceiver;
-
-        return aos::ErrorEnum::eNone;
-    }
-
-    aos::Error FinishProvisioning(const aos::String& password) override
+    Error FinishProvisioning(const String& password) override
     {
         mPassword = password;
 
-        return aos::ErrorEnum::eNone;
+        return ErrorEnum::eNone;
     }
 
-    aos::Error Deprovision(const aos::String& password) override
+    Error Deprovision(const String& password) override
     {
         mPassword = password;
 
-        return aos::ErrorEnum::eNone;
+        return ErrorEnum::eNone;
     }
 
-    aos::String GetPassword() const { return mPassword; }
-    aos::String GetCertType() const { return mCertType; }
-    aos::String GetSubject() const { return mSubject; }
-    aos::String GetCSR() const { return mCSR; }
-    aos::String GetCert() const { return mCert; }
+    String GetPassword() const { return mPassword; }
+    String GetCertType() const { return mCertType; }
+    String GetSubject() const { return mSubject; }
+    String GetCSR() const { return mCSR; }
+    String GetCert() const { return mCert; }
 
-    void SetCSR(const aos::String& csr) { mCSR = csr; }
-    void SetCertTypes(const aos::iam::provisionmanager::CertTypes& certTypes) { mCertTypes = certTypes; }
-    void SetCertInfo(const aos::iam::certhandler::CertInfo& certInfo) { mCertInfo = certInfo; }
+    void SetCSR(const String& csr) { mCSR = csr; }
+    void SetCertTypes(const iam::provisionmanager::CertTypes& certTypes) { mCertTypes = certTypes; }
+    void SetCertInfo(const iam::certhandler::CertInfo& certInfo) { mCertInfo = certInfo; }
 
 private:
-    aos::StaticString<aos::iam::certhandler::cPasswordLen> mPassword;
-    aos::iam::provisionmanager::CertTypes                  mCertTypes;
-    aos::StaticString<aos::iam::certhandler::cCertTypeLen> mCertType;
-    aos::StaticString<aos::crypto::cSubjectCommonNameLen>  mSubject;
-    aos::StaticString<aos::crypto::cCSRPEMLen>             mCSR;
-    aos::StaticString<aos::crypto::cCertPEMLen>            mCert;
-    aos::iam::certhandler::CertInfo                        mCertInfo;
+    StaticString<iam::certhandler::cPasswordLen> mPassword;
+    iam::provisionmanager::CertTypes             mCertTypes;
+    StaticString<iam::certhandler::cCertTypeLen> mCertType;
+    StaticString<crypto::cSubjectCommonNameLen>  mSubject;
+    StaticString<crypto::cCSRPEMLen>             mCSR;
+    StaticString<crypto::cCertPEMLen>            mCert;
+    iam::certhandler::CertInfo                   mCertInfo;
 };
+
+} // namespace aos::zephyr
 
 #endif
