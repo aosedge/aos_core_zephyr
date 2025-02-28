213df615eb8f87ff2f067e3c92f188c63dcbf9c6
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-02-28 13:17:35 +0200

    [tests,smclient] Sync resource manager stub with core lib interface
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- tests/smclient/src/stubs/resourcemanagerstub.hpp
+++ tests/smclient/src/stubs/resourcemanagerstub.hpp
@@ -10,44 +10,148 @@
 
 #include <aos/sm/resourcemanager.hpp>
 
+namespace aos::zephyr {
+
 class ResourceManagerStub : public aos::sm::resourcemanager::ResourceManagerItf {
 public:
-    aos::RetWithError<aos::StaticString<aos::cVersionLen>> GetNodeConfigVersion() const override { return mVersion; }
+    /**
+     * Returns current node config version.
+     *
+     * @return RetWithError<StaticString<cVersionLen>>.
+     */
+    RetWithError<StaticString<cVersionLen>> GetNodeConfigVersion() const override { return mVersion; }
 
-    aos::Error GetDeviceInfo(const aos::String& deviceName, aos::DeviceInfo& deviceInfo) const override
+    /**
+     * Returns node config.
+     *
+     * @param nodeConfig[out] param to store node config.
+     * @return Error.
+     */
+    Error GetNodeConfig(aos::NodeConfig& nodeConfig) const override
     {
-        return aos::ErrorEnum::eNone;
+        nodeConfig = *mNodeConfig;
+
+        return ErrorEnum::eNone;
     }
 
-    aos::Error GetResourceInfo(const aos::String& resourceName, aos::ResourceInfo& resourceInfo) const override
+    /**
+     * Gets device info by name.
+     *
+     * @param deviceName device name.
+     * @param[out] deviceInfo param to store device info.
+     * @return Error.
+     */
+    Error GetDeviceInfo(const String& deviceName, DeviceInfo& deviceInfo) const override
     {
-        return aos::ErrorEnum::eNone;
+        (void)deviceName;
+        (void)deviceInfo;
+
+        return ErrorEnum::eNone;
     }
 
-    aos::Error AllocateDevice(const aos::String& deviceName, const aos::String& instanceID) override
+    /**
+     * Gets resource info by name.
+     *
+     * @param resourceName resource name.
+     * @param[out] resourceInfo param to store resource info.
+     * @return Error.
+     */
+    Error GetResourceInfo(const String& resourceName, ResourceInfo& resourceInfo) const override
     {
-        return aos::ErrorEnum::eNone;
+        (void)resourceName;
+        (void)resourceInfo;
+
+        return ErrorEnum::eNone;
     }
 
-    aos::Error ReleaseDevice(const aos::String& deviceName, const aos::String& instanceID) override
+    /**
+     * Allocates device by name.
+     *
+     * @param deviceName device name.
+     * @param instanceID instance ID.
+     * @return Error.
+     */
+    Error AllocateDevice(const String& deviceName, const String& instanceID) override
     {
-        return aos::ErrorEnum::eNone;
+        (void)deviceName;
+        (void)instanceID;
+
+        return ErrorEnum::eNone;
     }
 
-    aos::Error ReleaseDevices(const aos::String& instanceID) override { return aos::ErrorEnum::eNone; }
-
-    aos::Error GetDeviceInstances(
-        const aos::String& deviceName, aos::Array<aos::StaticString<aos::cInstanceIDLen>>& instanceIDs) const override
+    /**
+     * Releases device for instance.
+     *
+     * @param deviceName device name.
+     * @param instanceID instance ID.
+     * @return Error.
+     */
+    Error ReleaseDevice(const String& deviceName, const String& instanceID) override
     {
-        return aos::ErrorEnum::eNone;
+        (void)deviceName;
+        (void)instanceID;
+
+        return ErrorEnum::eNone;
     }
 
-    aos::Error CheckNodeConfig(const aos::String& version, const aos::String& config) const override
+    /**
+     * Releases all previously allocated devices for instance.
+     *
+     * @param instanceID instance ID.
+     * @return Error.
+     */
+    Error ReleaseDevices(const String& instanceID) override
     {
-        return aos::ErrorEnum::eNone;
+        (void)instanceID;
+
+        return ErrorEnum::eNone;
     }
 
-    aos::Error UpdateNodeConfig(const aos::String& version, const aos::String& config) override
+    /**
+     * Resets allocated devices.
+     *
+     * @return Error.
+     */
+    Error ResetAllocatedDevices() override { return ErrorEnum::eNone; }
+
+    /**
+     * Returns ID list of instances that allocate specific device.
+     *
+     * @param deviceName device name.
+     * @param instances[out] param to store instance ID(s).
+     * @return Error.
+     */
+    Error GetDeviceInstances(const String& deviceName, Array<StaticString<cInstanceIDLen>>& instanceIDs) const override
+    {
+        (void)deviceName;
+        (void)instanceIDs;
+
+        return ErrorEnum::eNone;
+    }
+
+    /**
+     * Checks configuration.
+     *
+     * @param version config version
+     * @param config string with configuration.
+     * @return Error.
+     */
+    Error CheckNodeConfig(const String& version, const String& config) const override
+    {
+        (void)version;
+        (void)config;
+
+        return ErrorEnum::eNone;
+    }
+
+    /**
+     * Updates configuration.
+     *
+     * @param version config version.
+     * @param config string with configuration.
+     * @return Error.
+     */
+    Error UpdateNodeConfig(const String& version, const String& config) override
     {
         mVersion = version;
         mConfig  = config;
@@ -55,11 +159,52 @@ public:
         return aos::ErrorEnum::eNone;
     }
 
-    const aos::String& GetNodeConfig() const { return mConfig; }
+    /**
+     * Subscribes to current node config change.
+     *
+     * @param receiver node config receiver.
+     * @return Error.
+     */
+    Error SubscribeCurrentNodeConfigChange(aos::sm::resourcemanager::NodeConfigReceiverItf& receiver) override
+    {
+        (void)receiver;
+
+        return ErrorEnum::eNone;
+    }
+
+    /**
+     * Unsubscribes to current node config change.
+     *
+     * @param receiver node config receiver.
+     * @return Error.
+     */
+    Error UnsubscribeCurrentNodeConfigChange(aos::sm::resourcemanager::NodeConfigReceiverItf& receiver) override
+    {
+        (void)receiver;
+
+        return ErrorEnum::eNone;
+    }
+
+    /**
+     * Sets node config.
+     *
+     * @param nodeConfig node config.
+     */
+    void SetNodeConfig(const aos::NodeConfig& nodeConfig) { *mNodeConfig = nodeConfig; }
+
+    /**
+     * Returns node config string.
+     *
+     * @return String
+     */
+    String GetNodeConfigString() const { return mConfig; }
 
 private:
-    aos::StaticString<aos::cVersionLen>                             mVersion;
-    aos::StaticString<aos::sm::resourcemanager::cNodeConfigJSONLen> mConfig;
+    StaticString<cVersionLen>                                  mVersion;
+    StaticString<aos::sm::resourcemanager::cNodeConfigJSONLen> mConfig;
+    std::unique_ptr<aos::NodeConfig>                           mNodeConfig = std::make_unique<aos::NodeConfig>();
 };
+
+} // namespace aos::zephyr
 
 #endif
