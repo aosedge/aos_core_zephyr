47e52458a2a1b302ee78bf6fd7ef6cc9d0e20729
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-02-28 13:31:12 +0200

    [tests,storage] Use test suite fixture and fix compilation
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- tests/storage/src/main.cpp
+++ tests/storage/src/main.cpp
@@ -13,45 +13,87 @@
 
 #include "storage/storage.hpp"
 #include "utils/log.hpp"
+#include "utils/utils.hpp"
 
-using namespace aos::zephyr;
+namespace aos::zephyr {
 
-static const aos::Array<uint8_t> StringToDN(const char* str)
+/***********************************************************************************************************************
+ * Types
+ **********************************************************************************************************************/
+
+struct storage_fixture {
+    storage::Storage mStorage;
+};
+
+/***********************************************************************************************************************
+ * Static
+ **********************************************************************************************************************/
+
+namespace {
+
+constexpr auto cStoragePath = CONFIG_AOS_STORAGE_DIR;
+
+const aos::Array<uint8_t> StringToDN(const char* str)
 {
     return aos::Array<uint8_t>(reinterpret_cast<const uint8_t*>(str), strlen(str) + 1);
 }
 
-ZTEST_SUITE(storage, NULL, NULL, NULL, NULL, NULL);
+} // namespace
 
-ZTEST(storage, test_AddUpdateRemoveInstance)
+/***********************************************************************************************************************
+ * Setup
+ **********************************************************************************************************************/
+
+ZTEST_SUITE(
+    storage, NULL,
+    []() -> void* {
+        aos::Log::SetCallback(TestLogCallback);
+
+        assert(!String(cStoragePath).IsEmpty());
+
+        auto err = FS::ClearDir(cStoragePath);
+        zassert_true(err.IsNone(), "Failed to clear storage directory: err= %s", utils::ErrorToCStr(err));
+
+        auto fixture = new storage_fixture;
+
+        err = fixture->mStorage.Init();
+        zassert_true(err.IsNone(), "Can't initialize IAM client: %s", utils::ErrorToCStr(err));
+
+        return fixture;
+    },
+    NULL, NULL, NULL);
+
+/***********************************************************************************************************************
+ * Tests
+ **********************************************************************************************************************/
+
+ZTEST_F(storage, test_AddUpdateRemoveInstance)
 {
-    aos::Log::SetCallback(TestLogCallback);
+    storage::Storage& storage = fixture->mStorage;
 
-    storage::Storage storage;
+    aos::StaticArray<sm::launcher::InstanceData, 2> instances;
 
-    zassert_equal(storage.Init(), aos::ErrorEnum::eNone, "Failed to initialize storage");
-
-    aos::StaticArray<aos::InstanceInfo, 2> instances;
-
-    aos::InstanceInfo instanceInfo;
-    instanceInfo.mInstanceIdent.mInstance  = 1;
-    instanceInfo.mInstanceIdent.mServiceID = "service_id";
-    instanceInfo.mInstanceIdent.mSubjectID = "subject_id";
-    instanceInfo.mPriority                 = 1;
-    instanceInfo.mStoragePath              = "storage_path";
-    instanceInfo.mStatePath                = "state_path";
-    instanceInfo.mUID                      = 1;
+    sm::launcher::InstanceData instanceInfo;
+    instanceInfo.mInstanceID                             = "id1";
+    instanceInfo.mInstanceInfo.mInstanceIdent.mInstance  = 1;
+    instanceInfo.mInstanceInfo.mInstanceIdent.mServiceID = "service_id";
+    instanceInfo.mInstanceInfo.mInstanceIdent.mSubjectID = "subject_id";
+    instanceInfo.mInstanceInfo.mPriority                 = 1;
+    instanceInfo.mInstanceInfo.mStoragePath              = "storage_path";
+    instanceInfo.mInstanceInfo.mStatePath                = "state_path";
+    instanceInfo.mInstanceInfo.mUID                      = 1;
 
     instances.PushBack(instanceInfo);
 
-    aos::InstanceInfo instanceInfo2;
-    instanceInfo2.mInstanceIdent.mInstance  = 2;
-    instanceInfo2.mInstanceIdent.mServiceID = "service_id";
-    instanceInfo2.mInstanceIdent.mSubjectID = "subject_id";
-    instanceInfo2.mPriority                 = 2;
-    instanceInfo2.mStoragePath              = "storage_path";
-    instanceInfo2.mStatePath                = "state_path";
-    instanceInfo2.mUID                      = 2;
+    sm::launcher::InstanceData instanceInfo2;
+    instanceInfo2.mInstanceID                             = "id2";
+    instanceInfo2.mInstanceInfo.mInstanceIdent.mInstance  = 2;
+    instanceInfo2.mInstanceInfo.mInstanceIdent.mServiceID = "service_id";
+    instanceInfo2.mInstanceInfo.mInstanceIdent.mSubjectID = "subject_id";
+    instanceInfo2.mInstanceInfo.mPriority                 = 2;
+    instanceInfo2.mInstanceInfo.mStoragePath              = "storage_path";
+    instanceInfo2.mInstanceInfo.mStatePath                = "state_path";
+    instanceInfo2.mInstanceInfo.mUID                      = 2;
 
     instances.PushBack(instanceInfo2);
 
@@ -63,24 +105,24 @@ ZTEST(storage, test_AddUpdateRemoveInstance)
         zassert_equal(storage.AddInstance(instance), aos::ErrorEnum::eAlreadyExist, "Unexpected error");
     }
 
-    instanceInfo2.mPriority    = 3;
-    instanceInfo2.mStoragePath = "storage_path";
+    instanceInfo2.mInstanceInfo.mPriority    = 3;
+    instanceInfo2.mInstanceInfo.mStoragePath = "storage_path";
     zassert_equal(storage.AddInstance(instanceInfo2), aos::ErrorEnum::eAlreadyExist, "Unexpected error");
 
-    aos::StaticArray<aos::InstanceInfo, 2> instances2;
+    aos::StaticArray<sm::launcher::InstanceData, 2> instances2;
     zassert_equal(storage.GetAllInstances(instances2), aos::ErrorEnum::eNone, "Failed to get all instances");
 
     zassert_equal(instances2.Size(), 2, "Unexpected number of instances");
 
     for (const auto& instance : instances2) {
-        zassert_equal(instances.Find(instance).mError, aos::ErrorEnum::eNone, "Unexpected instance");
+        zassert_not_equal(instances.Find(instance), instances.end(), "Unexpected instance");
     }
 
-    instances[0].mStoragePath = "storage_path2";
-    instances[0].mStatePath   = "state_path2";
+    instances[0].mInstanceInfo.mStoragePath = "storage_path2";
+    instances[0].mInstanceInfo.mStatePath   = "state_path2";
 
-    instances[1].mStoragePath = "storage_path3";
-    instances[1].mStatePath   = "state_path3";
+    instances[1].mInstanceInfo.mStoragePath = "storage_path3";
+    instances[1].mInstanceInfo.mStatePath   = "state_path3";
 
     for (const auto& instance : instances) {
         zassert_equal(storage.UpdateInstance(instance), aos::ErrorEnum::eNone, "Failed to update instance");
@@ -92,12 +134,11 @@ ZTEST(storage, test_AddUpdateRemoveInstance)
     zassert_equal(instances2.Size(), 2, "Unexpected number of instances");
 
     for (const auto& instance : instances2) {
-        zassert_equal(instances.Find(instance).mError, aos::ErrorEnum::eNone, "Unexpected instance");
+        zassert_not_equal(instances.Find(instance), instances.end(), "Unexpected instance");
     }
 
     for (const auto& instance : instances) {
-        zassert_equal(
-            storage.RemoveInstance(instance.mInstanceIdent), aos::ErrorEnum::eNone, "Failed to remove instance");
+        zassert_equal(storage.RemoveInstance(instance.mInstanceID), aos::ErrorEnum::eNone, "Failed to remove instance");
     }
 
     instances2.Clear();
@@ -106,13 +147,9 @@ ZTEST(storage, test_AddUpdateRemoveInstance)
     zassert_equal(instances2.Size(), 0, "Unexpected number of instances");
 }
 
-ZTEST(storage, test_AddUpdateRemoveService)
+ZTEST_F(storage, test_AddUpdateRemoveService)
 {
-    aos::Log::SetCallback(TestLogCallback);
-
-    storage::Storage storage;
-
-    zassert_equal(storage.Init(), aos::ErrorEnum::eNone, "Failed to initialize storage");
+    storage::Storage& storage = fixture->mStorage;
 
     aos::StaticArray<aos::sm::servicemanager::ServiceData, 2> services;
 
@@ -152,19 +189,25 @@ ZTEST(storage, test_AddUpdateRemoveService)
     zassert_equal(services2.Size(), 2, "Unexpected number of services");
 
     for (const auto& service : services2) {
-        zassert_equal(services.Find(service).mError, aos::ErrorEnum::eNone, "Unexpected service");
+        zassert_not_equal(services.Find(service), services.end(), "Unexpected service");
     }
 
-    aos::StaticString<aos::cServiceIDLen> serviceID {"service_id"};
+    {
+        StaticArray<sm::servicemanager::ServiceData, 2> serviceVersions;
 
-    auto ret = storage.GetService(serviceID);
+        zassert_equal(storage.GetServiceVersions(serviceData.mServiceID, serviceVersions), aos::ErrorEnum::eNone,
+            "Failed to get service");
 
-    zassert_equal(ret.mError, aos::ErrorEnum::eNone, "Failed to get service");
+        LOG_MODULE_DBG("storage") << "serviceID: " << serviceData.mVersion.CStr()
+                                  << ", count=" << serviceVersions.Size();
 
-    printk("serviceID: %s\n", serviceData.mVersion.CStr());
-    printk("serviceID: %s\n", ret.mValue.mVersion.CStr());
+        zassert_equal(serviceVersions.Size(), 1, "Unexpected service versions");
 
-    zassert_equal(ret.mValue, serviceData, "Unexpected service");
+        printk("serviceID: %s\n", serviceData.mVersion.CStr());
+        printk("serviceID: %s\n", serviceVersions[0].mVersion.CStr());
+
+        zassert_equal(serviceVersions[0], serviceData, "Unexpected service");
+    }
 
     services[0].mImagePath = "image_path3";
     services[1].mImagePath = "image_path4";
@@ -179,7 +222,7 @@ ZTEST(storage, test_AddUpdateRemoveService)
     zassert_equal(services2.Size(), 2, "Unexpected number of services");
 
     for (const auto& service : services2) {
-        zassert_equal(services.Find(service).mError, aos::ErrorEnum::eNone, "Unexpected service");
+        zassert_not_equal(services.Find(service), services.end(), "Unexpected service");
     }
 
     for (const auto& service : services) {
@@ -193,13 +236,9 @@ ZTEST(storage, test_AddUpdateRemoveService)
     zassert_equal(services2.Size(), 0, "Unexpected number of services");
 }
 
-ZTEST(storage, test_AddRemoveCertInfo)
+ZTEST_F(storage, test_AddRemoveCertInfo)
 {
-    aos::Log::SetCallback(TestLogCallback);
-
-    storage::Storage storage;
-
-    zassert_equal(storage.Init(), aos::ErrorEnum::eNone, "Failed to initialize storage");
+    storage::Storage& storage = fixture->mStorage;
 
     aos::StaticArray<aos::iam::certhandler::CertInfo, 2> certInfos;
 
@@ -243,7 +282,7 @@ ZTEST(storage, test_AddRemoveCertInfo)
     zassert_equal(certInfos2.Size(), 2, "Unexpected number of cert infos");
 
     for (const auto& certInfo : certInfos2) {
-        zassert_equal(certInfos.Find(certInfo).mError, aos::ErrorEnum::eNone, "Unexpected cert info");
+        zassert_not_equal(certInfos.Find(certInfo), certInfos.end(), "Unexpected cert info");
     }
 
     zassert_equal(storage.AddCertInfo(certTypeTpm, certInfoTpm), aos::ErrorEnum::eNone, "Failed to add cert info");
@@ -287,3 +326,5 @@ ZTEST(storage, test_AddRemoveCertInfo)
     zassert_equal(storage.GetCertsInfo(certTypeTpm, certInfos2), aos::ErrorEnum::eNone, "Failed to get all cert infos");
     zassert_equal(certInfos2.Size(), 0, "Unexpected number of cert infos");
 }
+
+} // namespace aos::zephyr
