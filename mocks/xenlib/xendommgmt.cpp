19db8cf9ff42d61e0c4cd9c83793f98fd53d4e48
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-05-30 16:48:31 +0300

    [mocks] Implement xen mocks for native build
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

---
+++ mocks/xenlib/xendommgmt.cpp
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2025 EPAM Systems
+ */
+
+#include "xen_dom_mgmt.h"
+
+struct xen_domain* get_domain(uint32_t domid)
+{
+    (void)domid;
+
+    return nullptr;
+}
+
+uint32_t find_domain_by_name(char* arg)
+{
+    (void)arg;
+
+    return 0;
+}
